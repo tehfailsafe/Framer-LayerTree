4069bfb9ce21fe7dc24709700e6c5a720c66ed33
Mike <mike@failsafedesign.com>
2016-02-26 10:56:58 -0800

    Added files via upload

---
+++ LayerTree.js
@@ -0,0 +1,27 @@
+module.exports = (function() {
+  function exports() {}
+
+  exports.buildLayers = function(file) {
+    var layer, name, results;
+    results = [];
+    for (name in file) {
+      layer = file[name];
+      results.push(this.createChildNodes(layer));
+    }
+    return results;
+  };
+
+  exports.createChildNodes = function(layer) {
+    var child, index, ref, results;
+    ref = layer.children;
+    results = [];
+    for (index in ref) {
+      child = ref[index];
+      results.push(layer[child.name] = child);
+    }
+    return results;
+  };
+
+  return exports;
+
+})();
